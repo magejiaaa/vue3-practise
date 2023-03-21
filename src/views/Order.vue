9ff3542c5f3ae3a6f0f75011336e7d606cf3d15b
mageji_mac <dses60337@gmail.com>
2023-03-21 16:03:55 +0800

    修後台訂單+前台訂單

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -2,7 +2,7 @@
     <Loading :active="isLoading"></Loading>
     <Toast></Toast>
     <!-- 正文內容 -->
-    <table class="table mt-4">
+    <table class="table mt-4" v-if="AllProducts">
         <thead>
             <tr>
                 <th width="120">購買時間</th>
@@ -17,8 +17,10 @@
             <tr v-for="item in orders" :key="item.id">
                 <td>{{ $filters.date(item.create_at) }}</td>
                 <td>{{ item.user.email }}</td>
-                <td class="text-right" v-for="product in item.products" :key="product.id">
-                    {{ product.name && product.name.title }} / {{ product.qty }}{{ product.name && product.name.unit }}
+                <td class="text-right">
+                    <p class="mb-0" v-for="orderPro in item.products" :key="orderPro.id">
+                        {{ orderPro.product?.title }} / {{ orderPro.qty }}{{ orderPro.product?.unit }}
+                    </p>
                 </td>
                 <td class="text-right">
                     {{ $filters.currency(item.total) }}
@@ -52,57 +54,14 @@ import DelModal from '../components/DelModal.vue';
 import Toast from '../components/ToastMessages.vue';
 import Pages from '../components/PagesList.vue';
 
+import { mapState, mapActions } from 'pinia';
+import productStore from '@/stores/productStore';
+
 
 export default {
     data() {
         return {
-            orders: [
-                {
-                    "create_at": 1523539834,
-                    "id": "-L9u2EUkQSoEmW7QzGLF",
-                    "is_paid": true,
-                    "message": "這是留言",
-                    "paid_date": 1523539924,
-                    "payment_method": "credit_card",
-                    "products": {
-                        "L8nBrq8Ym4ARI1Kog4t": {
-                            "id": "L8nBrq8Ym4ARI1Kog4t",
-                            "product_id": "-NO8dU6u2Mze0vUU3Fny",
-                            "qty": "3"
-                        }
-                    },
-                    "total": 100,
-                    "user": {
-                        "address": "kaohsiung",
-                        "email": "test@gmail.com",
-                        "name": "test",
-                        "tel": "0912346768"
-                    },
-                    "num": 1
-                },
-                {
-                    "create_at": 1523539834,
-                    "id": "-L9u2EUkQSoEmW7QzGLF",
-                    "is_paid": false,
-                    "message": "這是留言",
-                    "products": {
-                        "L8nBrq8Ym4ARI1Kog4t": {
-                            "id": "L8nBrq8Ym4ARI1Kog4t",
-                            "product_id": "-NO8c9jF-eXmWCEoEhWi",
-                            "qty": "3"
-                        }
-                    },
-                    "total": 100,
-                    "user": {
-                        "address": "kaohsiung",
-                        "email": "test@gmail.com",
-                        "name": "test",
-                        "tel": "0912346768"
-                    },
-                    "num": 2
-                },
-            ],
-            productsList: [],
+            orders: [],
             pagination: {},
             tempOrder: {},
             isNew: false,
@@ -117,13 +76,11 @@ export default {
         Pages,
     },
     inject: ['emitter'],
+    computed: {
+        ...mapState(productStore, ['AllProducts']),
+    },
     methods: {
-        // 定義一個接受 productsList 參數的 getProductTitle 函數
-        getProductTitle(productId, productsList) {
-            // 判斷productsList中的id是否等於傳入的第一個值(productId)
-            const product = productsList.find((item) => item.id === productId);
-            return product ? product : "Unknown";
-        },
+        ...mapActions(productStore, ['getAllProducts']),
         getOrders(pages = 1) {
             this.pages = pages;
             const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/orders?page=${pages}`;
@@ -132,33 +89,12 @@ export default {
             this.$http.get(api)
                 .then((res) => {
                     if (res.data.success) {
-                        // this.orders = res.data.orders;
+                        this.isLoading = false;
+                        this.orders = res.data.orders;
                         this.pagination = res.data.pagination;
                         // 取得產品資料
-                        return new Promise((resolve, reject) => {
-                            const proApi = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/products`;
-                            this.$http.get(proApi)
-                                .then((resPro) => {
-                                    if (resPro.data.success) {
-                                        this.productsList = resPro.data.products;
-                                        resolve();
-                                    } else {
-                                        reject('產品資料取得失敗');
-                                    }
-                                })
-                        });
+                        this.getAllProducts();
                     }
-                })
-                .then(() => {
-                    this.isLoading = false;
-                    this.orders.forEach((order) => {
-                        // 帶入的product為每筆訂單的products
-                        // Object.values將order.products轉換成陣列
-                        Object.values(order.products).forEach((product) => {
-                            // 將符合product_id的產品寫入至product.name
-                            product.name = this.getProductTitle(product.product_id, this.productsList);
-                        });
-                    });
                 })
         },
         openModal(item) {
@@ -193,7 +129,7 @@ export default {
                 });
         },
         delProduct() {
-            const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/product/${this.tempProduct.id}`;
+            const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/order/${this.tempProduct.id}`;
             const delComponent = this.$refs.delModal;
             this.isLoading = true;
             // 第一個是路徑 第二個是送出的資料
--- src/views/UserCheckOut.vue
+++ src/views/UserCheckOut.vue
@@ -1,6 +1,95 @@
 <template>
-    <div class="my-5 row justify-content-center">
-        <form class="col-md-6" @submit.prevent="payOrder">
+    <div class="row | innerPage">
+        <div class="col-md-8">
+            <div class="cartTitle">
+                <router-link to>
+                    <button @click="this.$router.back(1)">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <path d="M17.77 3.77L16 2L6 12L16 22L17.77 20.23L9.54 12L17.77 3.77Z" fill="#828282" />
+                        </svg>
+                        <span>訂單列表</span>
+                    </button>
+                </router-link>
+                訂單詳情
+            </div>
+            <!-- 購物車商品 -->
+            <div class="cartCard | orderCard">
+                <div class="card" v-for="item in order.products" :key="item.product_id">
+                    <div class="card-body | cartCardContent">
+                        <div class="cardFirstBox">
+                            <div class="cartImg"
+                                :style="{ backgroundImage: `url(${item.product.imagesUrl ? item.product.imagesUrl[0] : item.product.imageUrl})` }">
+                            </div>
+                            <p class="card-text mb-0">{{ item.product.title }}</p>
+                        </div>
+                        <div class="d-flex | cartProductQty">
+                            {{ item.qty }}{{ item.product.unit }}
+                            <span style="line-height: 1.1em;">
+                                <small v-if="item.final_total !== item.total" class="text-success"
+                                    style="display: block;">折扣價</small>
+                                NT$ {{ $filters.currency(item.final_total) }}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- 訂單購買人資料 -->
+            <table class="orderUser">
+                <thead>購買人資訊</thead>
+                <tbody>
+                    <tr class="userTitle">
+                        <th>收件人</th>
+                        <td>{{ order.user.name }}</td>
+                    </tr>
+                    <tr class="userTitle">
+                        <th>電話</th>
+                        <td>{{ order.user.tel }}</td>
+                    </tr>
+                    <tr class="userTitle">
+                        <th>e-mail</th>
+                        <td>{{ order.user.email }}</td>
+                    </tr>
+                    <tr class="userTitle">
+                        <th>地址</th>
+                        <td>{{ order.user.address }}</td>
+                    </tr>
+                    <tr v-if="order.message">
+                        <th>留言</th>
+                        <td>{{ order.message }}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <!-- 訂單價格 -->
+            <div class="cartTotal">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="input-group">
+                        <input type="text" class="form-control" v-model="coupon_code" placeholder="請輸入優惠碼">
+                    </div>
+                    <div>
+                        <span>總計 NT$ </span>
+                        <span class="priceBox" :class="{ 'bigPrice': cart.final_total == cart.total }">{{
+                            $filters.currency(cart.total) }}</span>
+                    </div>
+                </div>
+                <!-- 有優惠券才會出現 -->
+                <div style="color: red;" v-if="cart.final_total !== cart.total">
+                    <div class="d-flex justify-content-between align-items-center my-2">
+                        <div class="input-group">
+                            <span>已套用優惠券！</span>
+                        </div>
+                        <div>
+                            <span>優惠券折扣 NT$ </span>
+                            <span class="priceBox">{{ $filters.currency(cart.final_total - cart.total) }}</span>
+                        </div>
+                    </div>
+                    <p class="text-end m-0">折扣後價格
+                        <span>NT$ </span>
+                        <span class="bigPrice">{{ $filters.currency(cart.final_total) }}</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <form class="col-md-4" @submit.prevent="payOrder">
             <table class="table align-middle">
                 <thead>
                     <th>品名</th>
@@ -72,21 +161,21 @@ export default {
         getOrders() {
             const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/order/${this.orderId}`;
             this.$http.get(url)
-            .then((res) => {
-                if (res.data.success) {
-                    this.order = res.data.order;
-                }
-            })
+                .then((res) => {
+                    if (res.data.success) {
+                        this.order = res.data.order;
+                    }
+                })
         },
         payOrder() {
             const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/pay/${this.orderId}`;
             this.$http.post(url)
-            .then((res) => {
-                console.log(res);
-                if (res.data.success) {
-                    this.getOrders();
-                }
-            })
+                .then((res) => {
+                    console.log(res);
+                    if (res.data.success) {
+                        this.getOrders();
+                    }
+                })
         }
     },
     created() {
